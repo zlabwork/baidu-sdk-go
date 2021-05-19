678fc27c1d187c87c7fb4ad08ec5bc0be2b6659a
Joe <joe@xxtime.com>
2021-05-19 11:56:57 +0800

    地标识别 & 图片路径支持网络地址与本地地址

--- vision/image_vision.go
+++ vision/image_vision.go
@@ -1,7 +1,10 @@
 package vision
 
 import (
+    "encoding/base64"
     "github.com/zlabwork/baidu-sdk-go/request"
+    "io/ioutil"
+    "os"
 )
 
 const (
@@ -22,7 +25,7 @@ func NewVision() *vision {
 
 // 场景检测
 // @link https://ai.baidu.com/ai-doc/IMAGERECOGNITION/Xk3bcxe21
-func (vi *vision) Scene(url string) ([]byte, error) {
+func (vi *vision) Scene(address string) ([]byte, error) {
 
     // 1. uri
     uri := "/rest/2.0/image-classify/v2/advanced_general"
@@ -33,8 +36,59 @@ func (vi *vision) Scene(url string) ([]byte, error) {
 
     // 3. body
     body := make(map[string]string)
-    body["url"] = url
     body["baike_num"] = "0"
+    if address[:4] == "http" {
+        body["url"] = address
+    } else {
+        f, err := os.Open(address)
+        if err != nil {
+            return nil, err
+        }
+        defer f.Close()
+        b, err := ioutil.ReadAll(f)
+        if err != nil {
+            return nil, err
+        }
+        encode := base64.StdEncoding
+        body["image"] = encode.EncodeToString(b)
+    }
+
+    // 4. result
+    resp, err := vi.cli.BuildRequest("POST", uri, header, body)
+    if err != nil {
+        return nil, err
+    }
+    return resp, nil
+}
+
+// 地标识别
+// @link https://ai.baidu.com/ai-doc/IMAGERECOGNITION/jk3bcxbih
+func (vi *vision) Landmark(address string) ([]byte, error) {
+
+    // 1. uri
+    uri := "/rest/2.0/image-classify/v1/landmark"
+
+    // 2. header
+    header := make(map[string]string)
+    header["Content-Type"] = "application/x-www-form-urlencoded"
+
+    // 3. body
+    body := make(map[string]string)
+    if address[:4] == "http" {
+        body["url"] = address
+    } else {
+        f, err := os.Open(address)
+        if err != nil {
+            return nil, err
+        }
+        defer f.Close()
+        b, err := ioutil.ReadAll(f)
+        if err != nil {
+            return nil, err
+        }
+        encode := base64.StdEncoding
+        body["image"] = encode.EncodeToString(b)
+    }
 
     // 4. result
     resp, err := vi.cli.BuildRequest("POST", uri, header, body)
